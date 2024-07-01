547d762639780257bf3f7ecd63913b7f2fa2427e
semedii <abdisamed2023@hotmail.com>
2024-07-01 20:37:46 +0300

    first phase

---
+++ scripts.js
@@ -0,0 +1,5 @@
+function toggleDrawer() {
+    var drawer = document.getElementById("myDrawer");
+    drawer.style.display === "block" ? drawer.style.display = "none" : drawer.style.display = "block";
+  }
+  
